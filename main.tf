1bebfbcacc5b2f3bdaf32558ace438071948c577
prakasr2021 <76855781+prakasr2021@users.noreply.github.com>
2021-07-13 15:05:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,12 +15,12 @@ variable "generation" {
   description = "The VPC Generation to target. Valid values are 2 or 1."
 }
 
-#provider "ibm" {
-  #ibmcloud_api_key      = var.api_key
-#  generation            = var.generation
-#  region                = var.region
- # ibmcloud_timeout      = 300
-#}
+provider "ibm" {
+  ibmcloud_api_key      = var.api_key
+  generation            = var.generation
+  region                = var.region
+  ibmcloud_timeout      = 300
+}
 
 ##############################################################################
 # Read/validate Region
